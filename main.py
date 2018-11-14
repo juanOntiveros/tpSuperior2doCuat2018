75758382d5598972dd3ca73b7df54575a2b9d974
juanOntiveros <ontiverosfuertes@yahoo.com.ar>
2018-11-14 13:39:07 -0300

    Implementacion de cambios en el programa.

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ COEFICIENTES = "A de Coeficientes"
 INDEPENDIENTES = "B de Terminos independientes"
 AFIRMATIVO = "SI"
 NEGATIVO = "NO"
+BIENVENIDA = "BIENVENIDO A SIEL"
+IMPRESION_DATOS = "IMPRESION DE LOS DATOS INGRESADOS"
+INGRESO_DATOS = "INGRESO DE DATOS"
+DESPEDIDA = "Gracias por usar SIEL!"
 
-MENU_PRINCIPAL="""Menu Principal
-------
+MENU_PRINCIPAL="""                                 MENU PRINCIPAL
+
 1.Norma 1
 2.Norma 2
 3.Norma Infinito
@@ -25,57 +29,64 @@ MENU_PRINCIPAL="""Menu Principal
 
 0.Salir """
 
-MENU_METODOS="""Menu de Metodos Disponibles
------
+MENU_METODOS="""                                 Menu de Metodos Disponibles
+
 1.Metodo de Jacobi
 2.Metodo de Gauss Seidel
 
 0.Volver al menu principal """
-
+	
 #Funciones
 
 def imprimirBienvenida():
 	"""Despliega la bienvenida del sistema."""
-	print "Bienvenido a SIEL"
-	
+	print 
+	print BIENVENIDA.center(100," ")
+	print
+	print "En esta aplicacion trabajaremos con sistemas del tipo A.X = B"
+		
 def imprimirDespedida():
 	"""Despliega la despedida del sistema."""
-	print "-----"
-	print "Gracias por usar SIEL!"
+	print 
+	print DESPEDIDA.center(100," ")
 	
 def imprimirMenuPrincipal():
 	"""Imprime el menu principal."""
 	print 
-	print MENU_PRINCIPAL
+	print MENU_PRINCIPAL.center(100," ")
 
 def imprimirVolviendoAlMenuPrincipal():
 	"""Imprime mensaje volviendo al menu principal."""
+	print
 	print "Volviendo al menu principal..."
-	print "-----"
+	print 
 
 def imprimirSistemaCargado(matrizCoeficientes, matrizIndependientes):
 	"""Imprime cuando el sistema fue cargado satisfactoriamente."""
+	print
+	print IMPRESION_DATOS.center(100," ")
 	imprimirMatrizDeCoeficientes(matrizCoeficientes)
+	print "La matriz de coeficientes es Estrictamente Dominante Diagonalmente."
 	imprimirMatrizIndependientes(matrizIndependientes)
-	print "-----"
-	print "Sistema de ecuaciones lineales cargado..."
-	print "Deplegando menu principal..."
+	print 
+
 
 def imprimirMenuMetodos():
 	"""Imprime el menu con los metodos disponibles para solucionar
 	el sistema de ecuaciones lineales."""
 	print 
-	print MENU_METODOS
+	print MENU_METODOS.center(100," ")
 	
 def imprimirMatrizDeCoeficientes(matrizCoeficientes):
 	"""Imprime la matriz de Coeficientes."""
-	print "-----"
+	print
 	print "La matriz de coeficientes es de la forma: "
 	print matrizCoeficientes
 	
 def imprimirMatrizIndependientes(matrizIndependientes):
 	"""Imprime la matriz de terminos independientes."""
-	print "-----"
+	print
+	print
 	print "La matriz columna de terminos independientes es de la forma: "
 	print matrizIndependientes
 	
@@ -96,22 +107,23 @@ def ingresoDeDatosDeLaMatriz(matriz, filas, columnas, nombre):
 	print
 	for i in range(filas):
 		for j in range(columnas):
-			matriz[i,j] = input("Ingrese el dato de la fila {} y la columna {}: ".format(i+UNO,j+UNO))		
+			matriz[i,j] = input("Ingrese el valor de la fila {}, columna {}: ".format(i+UNO,j+UNO))		
 	
 def ingresarVectorInicial(filas):
 	"""Dado una cantidad de filas, le pide al usuario que ingrese el vector
 	inicial con el cual se resolvera el metodo seleccionado y 
 	luego lo devuelve."""
 	vectorInicial = crearVectorDeCeros(filas)
-	print "-----"
+	print 
+	print "Vector Inicial	"
 	for i in range(filas):
 		vectorInicial[i,CERO] = input("Ingrese la componente {} del vector inicial: ".format(i+UNO))
 	return vectorInicial
 
 def ingresarCotaDeError():
 	"""Pide el ingreso de la cota de error para su posterior uso."""
-	print "-----"
-	cotaDeError = input("Ingrese la cota de error: ")
+	print 
+	cotaDeError = input("Ingrese la cota de error, por ej 0.001: ")
 	if (cotaDeError < CERO):
 		print "La cota no puede ser menor a cero. "
 		cotaDeError = ingresarCotaDeError()
@@ -119,14 +131,16 @@ def ingresarCotaDeError():
 	
 def ingresarCantidadDeDecimales():
 	"""Pide el ingreso de la cantidad de decimales para su posterior uso."""
-	print "-----"
+	print
 	return input("Ingrese la cantidad de decimales: ")
 	
 def ingresoDeNumeroDeFilasyColumnas(filas, columnas):
 	"""Funcion que pide el ingreso del numero de filas y de columnas."""
 	print
-	filas = int(input("Ingrese el numero de filas: "))
-	columnas = int(input("Ingrese el numero de columnas: "))
+	print INGRESO_DATOS.center(100," ")
+	print 
+	filas = int(input("Ingrese el numero de filas de A: "))
+	columnas = int(input("Ingrese el numero de columnas de A: "))
 	return verificarFilasyColumnas(filas,columnas)
 	
 def ingresoDeDatos():
@@ -230,11 +244,11 @@ def verificarFilasyColumnas(filas,columnas):
 	
 def crearMatrizDeCeros(filas, columnas):
 	"""Crea una matriz de ceros, dado un numero de filas y columnas."""
-	return numpy.zeros((filas, columnas))
+	return numpy.zeros((filas, columnas), dtype = int)
 	
 def crearVectorDeCeros(filas):
 	"""Crea un vector de ceros, dado un numero de filas."""
-	return numpy.zeros((filas, UNO))
+	return numpy.zeros((filas, UNO), dtype = int)
 	
 def calcularOrden(matriz):
 	"""Calcula el orden de la matriz cuadrada dada."""
@@ -354,14 +368,12 @@ def calcularNormaUno(matrizCoeficientes):
 		listaDeMaximos.append(CERO)
 		for i in range(n):
 			listaDeMaximos[j] += int(abs(matrizCoeficientes[i,j]))
-	print "----"
 	print "La norma 1 de la matriz A es {}".format(max(listaDeMaximos))
 	
 def calcularNormaDos(matrizCoeficientes):
 	"""Dada una matriz calcula la norma 2 de la misma."""
 	matrizCalculada = multiplicarTranspuestaPorMatrizNormal(matrizCoeficientes)
 	listaDeAutovalores = calcularLosAutovaloresDeUnaMatriz(matrizCalculada)
-	print "----"
 	print "La norma 2 de la matriz A es {}".format(calcularElMaximoValorModuloDeLosAutovalores(listaDeAutovalores))
 	
 def calcularNormaInfinito(matrizCoeficientes):
@@ -372,7 +384,6 @@ def calcularNormaInfinito(matrizCoeficientes):
 		listaDeMaximos.append(CERO)
 		for j in range(n):
 			listaDeMaximos[i] += int(abs(matrizCoeficientes[i,j]))
-	print "----"
 	print "La norma infinito de la matriz A es {}".format(max(listaDeMaximos))
 	
 def calcularNormaInfinitoParaVector(vector):
@@ -431,18 +442,22 @@ def imprimirListaEnTabla(lista, numeroDecimales):
 def imprimirVariables(listaConResultados, numeroDecimales):
 	"""Imprime por pantalla las variables resultado del metodo elegido"""
 	print
-	print " ----- Variables ----- "
+	VARIABLES = "Variables"
+	print VARIABLES.center(50," ")
 	imprimirListaEnTabla(listaConResultados, numeroDecimales)
 	
 def imprimirCriterioDeParo(listaConCriteriosDeCorte, numeroDecimales):
 	"""Imprime por pantalla el criterio de paro resultado del metodo elegido"""
-	print " ----- Criterio de Paro ----- "
+	CRITERIO = "Criterio de Paro"
+	print CRITERIO.center(50," ")
 	imprimirListaEnTabla(listaConCriteriosDeCorte, numeroDecimales)
 
 def imprimirConjuntoDeValoresQueSatisfaceElSistema(listaConResultados):
 	"""Imprime el conjunto de valores que satisfacen el sistema
 	resultado del metodo elegido"""
-	print " ----- Conjunto de valores que satisfacen el sistema ----- "
+	print
+	SOLUCION = "Conjunto de valores que satisfacen el sistema"
+	print SOLUCION.center(50," ")
 	resultado = listaConResultados[len(listaConResultados) - UNO]
 	for i in range(calcularOrden(resultado)):
 		print int(round(resultado[i, CERO], UNO)) ,
@@ -474,8 +489,8 @@ def calcularMetodoGaussSeidel(matrizCoeficientes, matrizIndependientes):
 		
 def seleccionarOpcion(minimo, maximo):
 	"""Dado un minimo y un maximo, pide el ingreso de una opcion. """
-	print "-----"
-	opcion = int(input("Su opcion elegida es: "))
+	print
+	opcion = int(input("Ingresar la opcion elegida: "))
 	if (opcion < minimo or opcion > maximo):
 		print "Opcion elegida incorrecta. Vuelva a intentarlo"
 		opcion = seleccionarOpcion(minimo, maximo)
@@ -500,6 +515,8 @@ def ejecutarMenuMetodos(matrizCoeficientes, matrizIndependientes):
 
 def accionarDecisionDelMenuPrincipal(opcion, matrizCoeficientes, matrizIndependientes):
 	"""Dada una opcion seleccionada para el menu principal, la ejecuta."""
+	print
+	print "Resultado:"
 	if (opcion == UNO):
 		calcularNormaUno(matrizCoeficientes)
 	if (opcion == DOS):
